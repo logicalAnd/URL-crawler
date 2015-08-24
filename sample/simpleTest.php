f8ea840eac5cbb8e9598db26a9f7dbce44a20894
ShotaArai <araishouta@sa.local>
2015-08-24 15:57:20 +0900

    DOM version
    Here's a DOM version from stack overflow.

--- sample/simpleTest.php
+++ sample/simpleTest.php
@@ -1,18 +1,40 @@
 <?php
-
-function crawl_page($url, $depth = 5) {
-    if($depth > 0) {
-        $html = file_get_contents($url);
-
-        preg_match_all('~<a.*?href="(.*?)".*?>~', $html, $matches);
-        var_dump($matches[1]);
-        foreach($matches[1] as $newurl) {
-           crawl_page($newurl, $depth - 1);
-        }
-        file_put_contents('results.txt', $newurl."\n\n".$html."\n\n", FILE_APPEND);
+function crawl_page($url, $depth = 5)
+{
+    static $seen = array();
+    if (isset($seen[$url]) || $depth === 0) {
+        return;
     }
+
+    $seen[$url] = true;
+
+    $dom = new DOMDocument('1.0');
+    @$dom->loadHTMLFile($url);
+
+    $anchors = $dom->getElementsByTagName('a');
+    foreach ($anchors as $element) {
+        $href = $element->getAttribute('href');
+        if (0 !== strpos($href, 'http')) {
+            $path = '/' . ltrim($href, '/');
+            if (extension_loaded('http')) {
+                $href = http_build_url($url, array('path' => $path));
+            } else {
+                $parts = parse_url($url);
+                $href = $parts['scheme'] . '://';
+                if (isset($parts['user']) && isset($parts['pass'])) {
+                    $href .= $parts['user'] . ':' . $parts['pass'] . '@';
+                }
+                $href .= $parts['host'];
+                if (isset($parts['port'])) {
+                    $href .= ':' . $parts['port'];
+                }
+                $href .= $path;
+            }
+        }
+        crawl_page($href, $depth - 1);
+    }
+    //echo "URL:",$url,PHP_EOL,"CONTENT:",PHP_EOL,$dom->saveHTML(),PHP_EOL,PHP_EOL;
+
+    echo "URL:",$url;
 }
-
-crawl_page('http://www.logicaland.co.jp', 3);
-
-?>
+crawl_page("http://logicaland.co.jp", 2);
