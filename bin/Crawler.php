957639518ecff16d3b80e9020a317f1c524c55e3
ShotaArai <araishouta@sa.local>
2015-08-25 10:36:18 +0900

    using PHPCwawl Framework
    quickstart test program

--- bin/Crawler.php
+++ bin/Crawler.php
@@ -1,8 +1,28 @@
 <?php
-
 /* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * 2015/08/25 arai
+ * 
+ * Framework:PHPCrawl http://phpcrawl.cuab.de/
  */
+include("../libs/PHPCrawler.class.php");
 
+class MyCrawler extends PHPCrawler 
+{ 
+  function handleDocumentInfo(PHPCrawlerDocumentInfo $PageInfo) 
+  { 
+    // Your code comes here! 
+    // Do something with the $PageInfo-object that 
+    // contains all information about the currently  
+    // received document. 
+
+    // As example we just print out the URL of the document 
+    echo $PageInfo->url."\n"; 
+  } 
+}
+$crawler = new MyCrawler(); 
+$crawler->setURL("www.logicaland.co.jp"); 
+$crawler->addContentTypeReceiveRule("#text/html#"); 
+$crawler->setCrawlingDepthLimit(2);
+// ... 
+
+$crawler->go();
